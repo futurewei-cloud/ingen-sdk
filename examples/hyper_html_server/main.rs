6ed668ccb0b25b44151f927321b477c557379313
r12f <r12f.code@gmail.com>
2022-10-05 00:26:28 -0400

    Ading hyper example.

---
+++ examples/hyper_html_server/main.rs
@@ -0,0 +1,65 @@
+use std::net::SocketAddr;
+
+use hyper::server::conn::Http;
+use tokio::net::TcpListener;
+
+use bytes::Bytes;
+use http_body_util::Full;
+use hyper::service::service_fn;
+use hyper::{Method, Recv, Request, Response, Result, StatusCode};
+
+static INDEX: &str = "/data/agent/examples/hyper_html_server/index.html";
+static NOTFOUND: &[u8] = b"Not Found";
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    pretty_env_logger::init();
+
+    let addr: SocketAddr = "127.0.0.1:1337".parse().unwrap();
+
+    let listener = TcpListener::bind(addr).await?;
+    println!("Listening on http://{}", addr);
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        tokio::task::spawn(async move {
+            if let Err(err) = Http::new()
+                .serve_connection(stream, service_fn(response_examples))
+                .await
+            {
+                println!("Failed to serve connection: {:?}", err);
+            }
+        });
+    }
+}
+
+async fn response_examples(req: Request<Recv>) -> Result<Response<Full<Bytes>>> {
+    println!("Request received: {:?}", req.uri());
+
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") | (&Method::GET, "/index.html") => simple_file_send(INDEX).await,
+        (&Method::GET, "/no_file.html") => {
+            // Test what happens when file cannot be be found
+            simple_file_send("this_file_should_not_exist.html").await
+        }
+        _ => Ok(not_found()),
+    }
+}
+
+/// HTTP status code 404
+fn not_found() -> Response<Full<Bytes>> {
+    Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(Full::new(NOTFOUND.into()))
+        .unwrap()
+}
+
+async fn simple_file_send(filename: &str) -> Result<Response<Full<Bytes>>> {
+    if let Ok(contents) = std::fs::read(filename) {
+        let body = contents.into();
+        return Ok(Response::new(Full::new(body)));
+    }
+
+    Ok(not_found())
+}
